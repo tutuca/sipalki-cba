c28236360855c4f952e49951f4c765b4a2310b07
Matías Iturburu <maturburu@gmail.com>
2011-08-05 12:27:45 -0300

    Diseño

--- urls.py
+++ urls.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, include, url
-from django.contrib import admin
-admin.autodiscover()
+from django.conf import settings
 
-urlpatterns = patterns('',
+urlpatterns = patterns('sipalki.views',
     # Examples:
-    url(r'^$', 'siplaki.views.index', name='index'),
-    # url(r'^siplaki/', include('siplaki.foo.urls')),
-
-
-     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'index', name='index'),
+)
+urlpatterns += patterns('',
+    (r'^static/(.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
--- views.py
+++ views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, redirect, get_object_or_404
+from sipalki.forms import ContactForm
 
 def index(request):
-    render(
+    form = ContactForm
+    return render(
         request,
         'index.html',
-        {}
+        {'form':form}
     )
