759b70422e55480ddb33213061a7e4205bffc35e
Matías Iturburu <maturburu@gmail.com>
2011-09-30 06:55:15 -0300

    Reemplazo lightbox por fancybox porque hace justo lo que quiero

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,10 +4,12 @@ $(document).ready(function(){
         $('#slider').remove();
         target = $(this).attr('href');
         slider = $('<div>').attr('id', 'slider');
-        $(slider).load(target);
-        $('#pictures').after(slider);
-        $('#slider').lightbox();
-        $('#slider a:first').click();
+        $(slider).load(target, function(){
+            $('footer').after(slider);
+            $('#sider').toggle('slow')
+            $('#slider a').fancybox();
+            $('#slider a:first').click();
+        });
         return false;
     })
 })
