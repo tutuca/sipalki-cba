989f470ba1092279c65c4fa01daed9b2d32d64f5
Matías Iturburu <maturburu@gmail.com>
2011-09-30 05:25:02 -0300

    Refactorizo las vistas y agrego el manejo de emails

---
+++ classes/main.php
@@ -0,0 +1,47 @@
+<?php
+class Main extends F3instance {
+    function home() {
+        $albums = array();
+        $params = array(
+           'user_id' => '67705846@N02',
+           'method' => 'flickr.photosets.getList',
+
+        );
+        $response = unserialize(Flickr::makeCall($params));
+
+        foreach ($response['photosets']['photoset'] as $value){
+            $params = array(
+               'photo_id' => $value['primary'],
+               'method' => 'flickr.photos.getInfo',
+            );
+            $photo = unserialize(Flickr::makeCall($params));
+            $albums[] = array(
+                'id'=>$value['id'],
+                'photo'=>$photo['photo'],
+            );
+        };
+        $this->set('albums', $albums);
+        $this->set('title', 'Bienvenido');
+
+        echo Template::serve('index.html');
+    }
+    
+    function album() {
+        $photos = array();
+        $params = array(
+            'photoset_id' => $this->get('PARAMS["album_id"]'),
+            'method' => 'flickr.photosets.getPhotos',
+            'extras' => 'url_m, url_t'
+        );
+        $response = unserialize(Flickr::makeCall($params));
+        foreach ($response['photoset']['photo'] as $value){
+            $photos[] = $value;
+        }
+        
+        $this->set('photos', $photos);
+        echo Template::serve('album.html');
+    }
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,66 +1,39 @@
 <?php
-require_once(__DIR__.'/lib/base.php');
 require_once(__DIR__.'/lib/flickr.php');
+
+$app=require_once(__DIR__.'/lib/base.php');
+$app->config('config.cfg');
 
 Flickr::setApikey('86e5f5039a05d7349f7e4d7ada8fa7c8');
 Flickr::setFormat(Flickr::PHP);
 
-F3::set('CACHE',FALSE);
-F3::set('DEBUG',4);
-F3::set('UI','templates/');
+$app->route('GET /', 'Main->home');
 
-F3::route('GET /', 
-    function() {
-        $albums = array();
-        /*
-        $params = array(
-           'user_id' => '67705846@N02',
-           'method' => 'flickr.photosets.getList',
+$app->route('GET /album/@album_id','Main->album');
 
+$app->route('POST /contacto', 
+    function() use($app){
+        
+        $mail=new SMTP(
+            $app->get('SMTP_SERVER'),
+            $app->get('SMTP_PORT'),
+            NULL,
+            $app->get('ADMIN_EMAIL'),
+            $app->get('ADMIN_PASSWORD')
         );
-        $response = unserialize(Flickr::makeCall($params));
+        $app->set('name',$_POST['name']);
+        $app->set('email',$_POST['email']);
+        $app->set('to',$app->get('ADMIN_EMAIL'));
+        $app->set('subject','[SPK_CBA] Nuevo mensaje desde nuestra web');
+        $app->set('message',$_POST['message']);
 
-        foreach ($response['photosets']['photoset'] as $value){
-            $params = array(
-               'photo_id' => $value['primary'],
-               'method' => 'flickr.photos.getInfo',
-            );
-            $photo = unserialize(Flickr::makeCall($params));
-            $albums[] = array(
-                'id'=>$value['id'],
-                'photo'=>$photo['photo'],
-            );
-        };
-        */
-        F3::set('albums', $albums);
-        F3::set('title', 'Bienvenido');
+        $mail->set('from','"'.$app->get('name').'"<'.$app->get('email').'>');
+        $mail->set('to',$app->get('to'));
+        $mail->set('subject','[SPK_CBA] Nuevo mensaje desde nuestra web');
 
-        echo Template::serve('index.html');
+        $mail->send(Template::serve('email.txt'));
     }
 );
 
-F3::route('GET /album/@album_id', 
-    function() {
-        $photos = array();
-        /*$params = array(
-            'photoset_id' => F3::get('PARAMS["album_id"]'),
-            'method' => 'flickr.photosets.getPhotos',
-            'extras' => 'url_m'
-        );
-        $photos = unserialize(Flickr::makeCall($params));
-        */
-        F3::set('title', 'Album');
-        F3::set('photoset', $photos);
-        echo Template::serve('album.html');
-    }
-);
-
-F3::route('POST /contacto', 
-    function() {
-        F3::set('title', 'Bienvenido');
-        echo Template::serve('index.html');
-    }
-);
-
-F3::run();
+$app->run();
 ?>
